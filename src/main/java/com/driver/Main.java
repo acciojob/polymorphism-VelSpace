872d4467497b485c230f6dc5a63115bf11be0390
Velsp <Velsp@192.168.0.107>
2022-10-10 00:05:48 +0530

    Ploymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
+
 public class Main {
+	
+	 static class Product{
+			
+		public int product(int x, int y) {
+			return x*y;
+		}
+		
+		public int product(int x, int y, int z){
+			return x*y*z;
+		}
+		
+		public double product(double x, double y){
+			return x*y;
+		}
+		
+	}
+	public static void main(String[] args) {
+		Product p = new Product();
+		System.out.println(p.product(10, 10));
+		System.out.println(p.product(10, 10, 20));
+		System.out.println(p.product(10.12, 10.24));
+	}
 
 }
